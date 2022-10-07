0ede061bf508aa092f29c503459a160e3c82c2fd
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-07 11:00:25 +0200

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -3,7 +3,7 @@ Realizar un programa que
 calcule el Factorial de un
 número
 '''
-def factorial(n):
+def factorial_while(n):
     # Inicializo las variables
     index = n
     res = 1
@@ -15,6 +15,13 @@ def factorial(n):
     
     return res
 
+def factorial_for(n):
+    res = 1
+    
+    for i in range(1,n+1):
+        res = res * i
+    return res
+
 n = int(input("Dime un número: "))
-print("El factorial del número ", n, " es ", factorial(n))
+print("El factorial del número ", n, " es ", factorial_for(n))
 
