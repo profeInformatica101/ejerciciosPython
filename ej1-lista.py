0a6f3f6a8c2d70691bf2455899254ffee77fa414
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-13 10:09:04 +0200

    Add files via upload

---
+++ ej1-lista.py
@@ -0,0 +1,19 @@
+'''
+Definir una función que devuelva una
+lista con 20 números random
+'''
+# lista_numeros()
+# devuelve por ejemplo ---> [2,4,3,4,5,17,3,4,7,9,10,12,15,16,18,29,98,87, 65, 30]
+# len(lista_numeros()) --> 20
+# type(lista_numeros)) --> <class 'list'>
+def lista_numeros():
+    pass
+
+'''
+Definir una función que dado una lista
+filtre los números pares
+'''
+# filtra_lista_pares([1,2,3,4])
+# devuelve ---> [2,4]
+def filtra_lista_pares(lst):
+    pass
